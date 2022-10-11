2b3c578557e0912c5f6f1fcf322531d5fd31b10f
Brian Kim <hk2473@nyu.edu>
2022-10-10 18:27:27 -0700

    initial commit

---
+++ app/src/main/java/com/example/firebasetutorial/FirstFragment.java
@@ -0,0 +1,60 @@
+package com.example.firebasetutorial;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.firebasetutorial.databinding.FragmentFirstBinding;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    FirebaseDatabase root;
+    DatabaseReference reference;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        EditText inputText = (EditText) view.findViewById(R.id.inputText);
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                root = FirebaseDatabase.getInstance();
+                reference = root.getReference("message");
+                reference.setValue(inputText.getText().toString());
+
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
---
+++ app/src/main/java/com/example/firebasetutorial/SecondFragment.java
@@ -0,0 +1,76 @@
+package com.example.firebasetutorial;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.firebasetutorial.databinding.FragmentSecondBinding;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+
+public class SecondFragment extends Fragment {
+
+    private FragmentSecondBinding binding;
+    FirebaseDatabase root;
+    DatabaseReference reference;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView textView = (TextView) view.findViewById(R.id.textview_second);
+
+        root = FirebaseDatabase.getInstance();
+        reference = root.getReference("message");
+
+        // Read from the database
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                textView.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                textView.setText("Error in retrieving your message!");
+                Log.w("SecondFragment", "Failed to read value.", error.toException());
+            }
+        });
+
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
